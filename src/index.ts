77a7086825f1c096b22e0f470cb1b0b8c43f1365
Morgan Dilling <morgandilling171@gmail.com>
2024-03-04 14:51:49 +0000

    Revert "feat: Initial commit"
    This reverts commit d7176bc7a0783ca48ca70de586cd6f84c3ed6823.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
-import Calculator from '@lib/Calculator';
-
-const calc = new Calculator('5 2 4 * +');
-
-console.log(calc.evaluate());
+console.log('Hello World');
--- src/lib/Calculator.test.ts
+++
@@ -1,8 +0,0 @@
-import Calculator from './Calculator';
-
-describe('Calculator', () => {
-  it('should evaluate postfix expression', () => {
-    const calc = new Calculator('5 2 4 * +');
-    expect(calc.evaluate()).toBe(13);
-  });
-});
